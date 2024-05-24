cc1491f41929b52f39030758a7570954dfee0839
RcnonS <ratchanon.somprasong@gmail.com>
2024-05-24 13:57:50 +0700

    ex6

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,7 @@
 let height = undefined;
 let result;
 
+const heightResult = (height) => height ?? "Height is not defined";
+result = heightResult();
+
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
